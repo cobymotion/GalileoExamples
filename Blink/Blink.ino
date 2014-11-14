d674293e01b6c6593c295008d16db0f31808e961
Cobymotion <coby10@gmail.com>
2014-11-14 13:16:39 -0600

    Codigo de blink de galileo

---
+++ Blink/Blink.ino
@@ -0,0 +1,15 @@
+int led=13; 
+
+void setup() {
+ 
+  pinMode(led,OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly: 
+  digitalWrite(led,HIGH);
+  delay(1000); 
+  digitalWrite(led,LOW);
+  delay(1000);  
+}
